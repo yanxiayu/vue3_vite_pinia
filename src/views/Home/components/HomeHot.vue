dc80e14580aac15cd9a9e7db7c40ca6ee05de83b
yanxiayu <yanxiayu20@dnui.edu.cn>
2023-09-14 00:15:16 +0800

    图片懒加载的实现

--- src/views/Home/components/HomeHot.vue
+++ src/views/Home/components/HomeHot.vue
@@ -6,7 +6,7 @@
     <ul class="goods-list">
       <li v-for="item in hotList" :key="item.id">
         <RouterLink to="/">
-          <img :src="item.picture" alt="" />
+          <img v-img-lazy="item.picture" alt="" />
           <p class="name">{{ item.title }}</p>
           <p class="desc">{{ item.alt }}</p>
         </RouterLink>
