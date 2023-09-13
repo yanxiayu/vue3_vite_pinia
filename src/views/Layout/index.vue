4a947ca783a5d6683569a32f2af8230dc43ad7b3
yanxiayu <yanxiayu20@dnui.edu.cn>
2023-09-13 19:47:10 +0800

    准备导航吸顶组件以及实现交互效果

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <LayoutFixed />
     <LayoutNav />
     <LayoutHeader />
     <!--二级路由的出口  -->
@@ -12,6 +13,7 @@
 import LayoutNav from "./components/LayoutNav.vue";
 import LayoutHeader from "./components/LayoutHeader.vue";
 import LayoutFooter from "./components/LayoutFooter.vue";
+import LayoutFixed from "./components/LayoutFixed.vue";
 </script>
 
 <style></style>
