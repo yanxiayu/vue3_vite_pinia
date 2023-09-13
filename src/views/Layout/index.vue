ec1622a7240d79e9b994c03097c6fa572d7b3e59
yanxiayu <yanxiayu20@dnui.edu.cn>
2023-09-13 18:51:59 +0800

    静态结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    首页
+    <LayoutNav />
+    <LayoutHeader />
     <!--二级路由的出口  -->
     <router-view></router-view>
+    <LayoutFooter />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import LayoutNav from "./components/LayoutNav.vue";
+import LayoutHeader from "./components/LayoutHeader.vue";
+import LayoutFooter from "./components/LayoutFooter.vue";
+</script>
 
 <style></style>
