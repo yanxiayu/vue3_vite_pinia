56b8bd69cdaf653b202ee819c6e7433492ccafd1
yanxiayu <yanxiayu20@dnui.edu.cn>
2023-09-13 22:41:37 +0800

    Home组件的封装复用

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -5,7 +5,14 @@
   </div>
   <HomeNew />
   <HomeHot />
-  <homeProduct />
+  <HomeProduct />
+  <!-- 测试面板组件 -->
+  <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+    <div>新鲜好物插槽</div>
+  </HomePanel>
+  <HomePanel title="人气推荐" sub-title="人气推荐 好多商品">
+    <div>人气推荐插槽</div>
+  </HomePanel>
 </template>
 
 <script setup>
@@ -13,7 +20,8 @@ import HomeCategory from "./components/HomeCategory.vue";
 import HomeBanner from "./components/HomeBanner.vue";
 import HomeNew from "./components/HomeNew.vue";
 import HomeHot from "./components/HomeHot.vue";
-import homeProduct from "./components/HomeProduct.vue";
+import HomeProduct from "./components/HomeProduct.vue";
+import HomePanel from "./components/HomePanel.vue";
 </script>
 
 <style></style>
