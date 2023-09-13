2235b91a2bcb53d216c14590e6a91cbef029096a
yanxiayu <yanxiayu20@dnui.edu.cn>
2023-09-13 18:44:17 +0800

    配置sass变量的自动导入

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <!-- 一级路由出口组件 -->
   <router-view></router-view>
+  <div class="test">test scss</div>
 </template>
 <script setup></script>
-<style scoped></style>
+
+<style lang="scss" scoped>
+.test {
+  color: $xtxColor;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,10 @@ import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
+  plugins: [
+    vue(),
     // 配置插件
     AutoImport({
       resolvers: [ElementPlusResolver()],
@@ -18,7 +18,8 @@ export default defineConfig({
     Components({
       resolvers: [
         // 1.配置elementPlus采用sass样式配色系统
-        ElementPlusResolver({importStyle: 'sass'})],
+        ElementPlusResolver({ importStyle: "sass" }),
+      ],
     }),
   ],
   resolve: {
@@ -33,8 +34,9 @@ export default defineConfig({
         // 2.自动导入定制化样式文件进行样式覆盖
         additionalData: `
           @use "@/styles/element/index.scss" as *;
+          @use "@/styles/var.scss" as *;
         `,
-      }
-    }
-  }
+      },
+    },
+  },
 });
