68a9b3142500860af59850a642bd859aeee528b3
yanxiayu <yanxiayu20@dnui.edu.cn>
2023-09-14 18:20:41 +0800

    二级分类-面包屑导航实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -1,11 +1,22 @@
 // 因为http封装的axios是默认导出，所以这里可以自己命名
 import request from "@/utils/http";
 
+// 获取分类
 export function getCategoryAPI(id) {
   return request({
-    url: '/category',
+    url: "/category",
     params: {
-      id
-    }
-  })
+      id,
+    },
+  });
+}
+
+// 获取二级分类 分类id
+export function getCategoryFilterAPI(id) {
+  return request({
+    url: "/category/sub/filter",
+    params: {
+      id,
+    },
+  });
 }
--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -4,8 +4,10 @@
     <div class="bread-container">
       <el-breadcrumb separator=">">
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item :to="{ path: '/' }">居家 </el-breadcrumb-item>
-        <el-breadcrumb-item>居家生活用品</el-breadcrumb-item>
+        <el-breadcrumb-item :to="{ path: `/category/${categoryData.parentId}` }"
+          >{{ categoryData.parentName }}
+        </el-breadcrumb-item>
+        <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
     <div class="sub-container">
@@ -22,7 +24,20 @@
 </template>
 
 <script setup>
+import { getCategoryFilterAPI } from "@/apis/category.js";
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
 
+// 获取面包屑导航数据
+const categoryData = ref({});
+const route = useRoute();
+const getCategoryData = async () => {
+  const res = await getCategoryFilterAPI(route.params.id);
+  categoryData.value = res.result;
+};
+onMounted(() => {
+  getCategoryData();
+});
 </script>
 
 <style lang="scss" scoped>
