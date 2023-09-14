bba4fe4deec015bdeaba25d8b29e16781bbc2fb7
yanxiayu <yanxiayu20@dnui.edu.cn>
2023-09-14 10:57:04 +0800

    Home组件中product组件的列表渲染

--- src/apis/home.js
+++ src/apis/home.js
@@ -3,20 +3,27 @@ import httpInstance from "@/utils/http";
 // 获取banner接口
 export function getBannerAPI() {
   return httpInstance({
-    url:'/home/banner',
-  })
+    url: "/home/banner",
+  });
 }
 
 //获取新鲜好物
 export function findNewAPI() {
   return httpInstance({
-    url:'/home/new'
-  })
+    url: "/home/new",
+  });
 }
 
 // 获取人气推荐接口
 export function findHotAPI() {
   return httpInstance({
-    url:'/home/hot'
-  })
+    url: "/home/hot",
+  });
+}
+
+// 获取所有商品模块
+export function getGoodsAPI() {
+  return httpInstance({
+    url: "/home/goods",
+  });
 }
