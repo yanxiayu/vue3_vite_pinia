e69d0dc1a1f0f01edbf28d83f1164c3aeb5f9bae
yanxiayu <yanxiayu20@dnui.edu.cn>
2023-09-14 19:33:54 +0800

    详情页-梳理整体业务和路由的配置

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Layout from "@/views/Layout/index.vue";
 import Home from "@/views/Home/index.vue";
 import Category from "@/views/Category/index.vue";
 import SubCategory from "@/views/SubCategory/index.vue";
+import Detail from "@/views/Detail/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -29,6 +30,10 @@ const router = createRouter({
           path: "category/sub/:id",
           component: SubCategory,
         },
+        {
+          path: "detail/:id",
+          component: Detail,
+        },
       ],
     },
     {
@@ -36,7 +41,7 @@ const router = createRouter({
       component: Login,
     },
   ],
-  // 路由滚动行为定位
+  // 路由滚动行为定制
   scrollBehavior() {
     return {
       top: 0,
--- src/views/Home/components/HomeNew.vue
+++ src/views/Home/components/HomeNew.vue
@@ -5,7 +5,7 @@
     <!-- 插槽slot中的内容 -->
     <ul class="goods-list">
       <li v-for="item in newList" :key="item.id">
-        <RouterLink to="/">
+        <RouterLink :to="`/detail/${item.id}`">
           <img :src="item.picture" alt="" />
           <p class="name">{{ item.name }}</p>
           <p class="price">&yen;{{ item.price }}</p>
