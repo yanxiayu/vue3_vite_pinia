8d24cf736ef0b5b57d96f97f99f30e773396d19c
yanxiayu <yanxiayu20@dnui.edu.cn>
2023-09-14 22:09:04 +0800

    放大镜的效果-滑块跟随鼠标移动

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -4,7 +4,7 @@
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
@@ -33,7 +33,8 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref ,watch} from "vue";
+import { useMouseInElement } from "@vueuse/core";
 // 图片列表
 const imageList = [
   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -43,12 +44,37 @@ const imageList = [
   "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg",
 ];
 
-// 小图切换大图显示
+// 1.小图切换大图显示
 const activeIndex = ref(0);
-
 const enterhandle = (i) => {
   activeIndex.value = i;
 };
+
+// 2.获取鼠标相对位置
+const target = ref(null);
+const { elementX, elementY, isOutside } = useMouseInElement(target);
+
+// 3.控制滑块跟随鼠标移动（监听elementX和elementY的变化，一旦变化，重新设置left/top）
+const left = ref(0);
+const top = ref(0);
+watch([elementX, elementY], () => {
+  // 有效范围内控制滑块距离
+  // 横向
+  if (elementX.value > 100 && elementX.value < 300) {
+    left.value = elementX.value - 100;
+  }
+  // 纵向
+  if (elementY.value > 100 && elementY.value < 300) {
+    top.value = elementY.value - 100;
+  }
+
+  // 处理边界
+  if (elementX.value > 300) {left.value = 200};
+  if (elementX.value < 100) {left.value = 0};
+
+  if (elementY.value > 300) {top.value = 200};
+  if (elementY.value < 100) {top.value = 0};
+});
 </script>
 
 <style scoped lang="scss">
