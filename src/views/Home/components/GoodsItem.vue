84b736093efc3970dfbb8e96bb2d6a89e41b865c
yanxiayu <yanxiayu20@dnui.edu.cn>
2023-09-14 11:11:04 +0800

    product组件的goodsItem封装优化

---
+++ src/views/Home/components/GoodsItem.vue
@@ -0,0 +1,59 @@
+<!-- 纯展示类组件，这类组件的封装思想是：抽象props参数，传什么就显示什么
+     类似函数，定义好形参，实参传过来什么就是什么
+-->
+<template>
+  <RouterLink to="/" class="goods-item">
+    <img v-img-lazy="goods.picture" alt="" />
+    <p class="name ellipsis">{{ goods.name }}</p>
+    <p class="desc ellipsis">{{ goods.desc }}</p>
+    <p class="price">&yen;{{ goods.price }}</p>
+  </RouterLink>
+</template>
+
+<script setup>
+ defineProps({
+  goods: {
+    type: Object,
+    // 对象的默认值需要将他作为一个工厂函数返回
+    default: () => {},
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+ .goods-item {
+      display: block;
+      width: 220px;
+      padding: 20px 30px;
+      text-align: center;
+      transition: all 0.5s;
+
+      &:hover {
+        transform: translate3d(0, -3px, 0);
+        box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
+      }
+
+      img {
+        width: 160px;
+        height: 160px;
+      }
+
+      p {
+        padding-top: 10px;
+      }
+
+      .name {
+        font-size: 16px;
+      }
+
+      .desc {
+        color: #999;
+        height: 29px;
+      }
+
+      .price {
+        color: $priceColor;
+        font-size: 20px;
+      }
+    }
+</style>
--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -10,13 +10,8 @@
           </strong>
         </RouterLink>
         <ul class="goods-list">
-          <li v-for="good in cate.goods" :key="good.id">
-            <RouterLink to="/" class="goods-item">
-              <img v-img-lazy="good.picture" alt="" />
-              <p class="name ellipsis">{{ good.name }}</p>
-              <p class="desc ellipsis">{{ good.desc }}</p>
-              <p class="price">&yen;{{ good.price }}</p>
-            </RouterLink>
+          <li v-for="goods in cate.goods" :key="goods.id">
+            <GoodsItem :goods="goods"></GoodsItem>
           </li>
         </ul>
       </div>
@@ -26,7 +21,8 @@
 
 <script setup>
 import { getGoodsAPI } from "@/apis/home.js";
-import HomePanel from './HomePanel.vue'
+import HomePanel from "./HomePanel.vue";
+import GoodsItem from "./GoodsItem.vue";
 import { ref, onMounted } from "vue";
 
 // 获取数据列表
@@ -126,42 +122,6 @@ onMounted(() => {
         &:nth-child(4n) {
           margin-right: 0;
         }
-      }
-    }
-
-    .goods-item {
-      display: block;
-      width: 220px;
-      padding: 20px 30px;
-      text-align: center;
-      transition: all 0.5s;
-
-      &:hover {
-        transform: translate3d(0, -3px, 0);
-        box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
-      }
-
-      img {
-        width: 160px;
-        height: 160px;
-      }
-
-      p {
-        padding-top: 10px;
-      }
-
-      .name {
-        font-size: 16px;
-      }
-
-      .desc {
-        color: #999;
-        height: 29px;
-      }
-
-      .price {
-        color: $priceColor;
-        font-size: 20px;
       }
     }
   }
