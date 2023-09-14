95c5bfa163dd72620027af940691ab0ad0380112
yanxiayu <yanxiayu20@dnui.edu.cn>
2023-09-14 18:48:49 +0800

    二级分类-列表通过tab进行筛选功能的实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -11,14 +11,18 @@
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
       <div class="body">
         <!-- 商品列表-->
-        <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id"></GoodsItem>
+        <GoodsItem
+          v-for="goods in goodList"
+          :goods="goods"
+          :key="goods.id"
+        ></GoodsItem>
       </div>
     </div>
   </div>
@@ -48,15 +52,22 @@ const reqData = ref({
   categoryId: route.params.id,
   page: 1,
   pageSize: 20,
-  sortField:'publishTime'
+  sortField: "publishTime",
 });
 const getGoodList = async () => {
   const res = await getSubCategoryAPI(reqData.value);
-  goodList.value = res.result.items
+  goodList.value = res.result.items;
 };
 onMounted(() => {
   getGoodList();
 });
+
+// tab切换回调
+const tabChange = () => {
+  // console.log('tab切换了',reqData.value.sortField);
+  reqData.value.page = 1;
+  getGoodList();
+};
 </script>
 
 <style lang="scss" scoped>
